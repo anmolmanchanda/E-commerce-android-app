752011357c40a3ce7aad325fa6bcc6fa11a9188e
Anmol Manchanda <Anmolmanchanda5560@conestogac.on.ca>
2022-04-16 16:56:41 -0400

    Splash Screen working

--- app/src/main/java/com/example/finalproject/SplashScreenActivity.java
+++ app/src/main/java/com/example/finalproject/SplashScreenActivity.java
@@ -4,13 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreenActivity extends AppCompatActivity {
-
+    private static int SPLASH_TIME_OUT = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
-        finish();
+
+        new Handler().postDelayed(() -> {
+            // This method will be executed once the timer is over
+            startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+            // close this activity
+            finish();
+        }, SPLASH_TIME_OUT);
     }
 }
